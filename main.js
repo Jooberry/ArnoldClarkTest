f9c2197d3efa3544c772361083339c22f0454b73
Jooberry <eunjooyoon@hotmail.co.uk>
2018-05-30 23:53:33 +0100

    changed age in seconds input box to birthdate input. Made relevant changes in code to accommodate this

--- main.js
+++ main.js
@@ -10,6 +10,7 @@
 // });
 
 var earthYrInSecs = 31557600
+var earthYrInDays = 365.25
 
 var orbitalPeriods = {
   mercury: 0.2408467,
@@ -24,16 +25,26 @@ var orbitalPeriods = {
 
 var handleButtonClick = function() {
   var input = document.querySelector("input");
+  var parsedInputDate = Date.parse(input.value);
+  // console.log(parsedInputDate);
+  var dateNow = new Date();
+  var parsedDateNow = Date.parse(dateNow.toISOString().substring(0,10));
+  // console.log(parsedDateNow);
+  var timeDiff = Math.abs(parsedDateNow - parsedInputDate);
+  // console.log(timeDiff);
+  var daysDiff = Math.ceil(timeDiff / (1000 * 3600 * 24));
+  // console.log(daysDiff);
+
   var select = document.getElementById("planet-select");
   var pTag = document.querySelector("#age-result");
+  var earthAgeCalculation = Math.floor(daysDiff / earthYrInDays);
   if (select.value === "Earth") {
-    var earthAgeCalculation = Math.floor(input.value / earthYrInSecs);
     return pTag.innerText = "On " + select.value + " you're the grand old age of " + earthAgeCalculation + " !";
   } else if (select.value === "Pick a planet") {
-    return pTag.innerText = "You space cadet! You need to pick a planet first... sigh...";
+    return pTag.innerText = "What a space cadet! You need to pick a planet first... sigh...";
   } else {
     var lowerCasePlanet = select.value.toLowerCase();
-    var planetAge = Math.floor((input.value / earthYrInSecs) * orbitalPeriods[lowerCasePlanet]);
+    var planetAge = Math.floor(earthAgeCalculation * orbitalPeriods[lowerCasePlanet]);
     return pTag.innerText = "On " + select.value + " you're the grand old age of " + planetAge + " !";
   }
 }
